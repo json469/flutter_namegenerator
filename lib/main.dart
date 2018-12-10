ca31d9d8382f7d965e0e5de2369b2171ffd2324d
Jesse <json469@aucklanduni.ac.nz>
2018-12-10 23:32:47 +1300

    Move codebase to separate randomwords.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'randomwords.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator')
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: _listViewItemBuilder,
-    );
-  }
-
-  Widget _listViewItemBuilder(BuildContext context, int i) {
-    if (i.isOdd) { return Divider(); }
-    final index = i ~/ 2;
-    if (index >= _suggestions.length) {
-      _suggestions.addAll(generateWordPairs().take(10));
-    }
-    return _buildRow(_suggestions[index]);
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
     );
   }
 }
---
+++ lib/randomwords.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: _listViewItemBuilder,
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int i) {
+    if (i.isOdd) { return Divider(); }
+    final index = i ~/ 2;
+    if (index >= _suggestions.length) {
+      _suggestions.addAll(generateWordPairs().take(10));
+    }
+    return _buildRow(_suggestions[index]);
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
